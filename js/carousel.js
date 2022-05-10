d14586e231cbdeff4617cf6ac399f06737c49e11
Leonardo Ari Santos Menegazzo <96697709+LeoMenegazzo@users.noreply.github.com>
2022-05-10 08:14:09 -0400

    123

--- js/carousel.js
+++ js/carousel.js
@@ -2,8 +2,22 @@ $(document).ready(function() {
     $('.wq-all-imgs-mobile').slick({
         dots: true,
         arrows: false,
-        infinite: false,
+        infinite: true,
         speed: 300,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    $('.wq-ui-imgs-carrousel').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
         slidesToShow: 1,
         slidesToScroll: 1,
     });
